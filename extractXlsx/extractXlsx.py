ecd10365e55c40e3b4b9e50591168547371420e4
Yuu <105552059+yuuyuki@users.noreply.github.com>
2022-05-21 22:47:18 +0800

    Extract Xlsx

---
+++ extractXlsx/extractXlsx.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import os
+import xlsxwriter       # need to pip install xlsxwriter
+import glob
+
+jpn_folder = "jpn"
+eng_folder = "eng"
+out_folder = 'out'
+
+files = os.listdir(jpn_folder)
+
+for file in files:
+    print(file)
+    d = {'Line': [], 'Jpn':[], 'Eng':[]}
+
+    jpn_file = os.path.join(jpn_folder, file)
+    try:
+        eng_file = glob.glob(os.path.join(eng_folder, file))[0]
+    except IndexError:
+        eng_file = None
+    out_file = os.path.join(out_folder, file + '.xlsx')
+
+    if not os.path.isfile(jpn_file):
+        continue
+    
+    ext = os.path.splitext(file)[-1].lower()
+
+    if ext != ".src":
+        continue
+    
+    with open(jpn_file, encoding='shift_jis') as f:
+        jpn_lines = f.readlines()
+    eng_lines = None
+    if eng_file is not None:
+        with open(eng_file, encoding='shift_jis') as g:
+            eng_lines = g.readlines()
+        if len(eng_lines) != len(jpn_lines):
+            print("Unequal number of lines for japan and english for " + file + ". Engilsh not parsed.")
+
+    for line_no, line in enumerate(jpn_lines):
+        if not line or line == '' or line =='\n' or line.isspace() or line[0] == ';' or line[0] == '#':
+            pass
+        else:
+            d['Line'].append(line_no)
+            d['Jpn'].append(line)
+            if eng_lines:
+                d['Eng'].append(eng_lines[line_no])
+            else:
+                d['Eng'].append('')  # machine translate
+                pass
+
+    df = pd.DataFrame(d)
+    df.set_index('Line', inplace=True)
+    writer = pd.ExcelWriter(out_file, engine='xlsxwriter')
+    df.to_excel(writer, sheet_name=file)
+    writer.save()
