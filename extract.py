d5a2bbd7fcabed48d84d3b7871a5c79f86dc11b3
Yuu <105552059+yuuyuki@users.noreply.github.com>
2022-05-22 00:52:00 +0800

    Write python to insert and extract excel files and Translate jyo01 a, b (#7)

---
+++ extract.py
@@ -0,0 +1,27 @@
+import os
+input_folder = 'script'
+output_folder = 'output'
+files = os.listdir(input_folder)
+
+for file in files:
+    tsv_file = ''     # tab separator
+
+    filepath = os.path.join(input_folder, file)
+    if os.path.isfile(filepath):
+        ext = os.path.splitext(filepath)[-1].lower()
+        if ext == ".src":
+            with open(filepath, encoding='shift_jis') as f:
+                lines = f.readlines()
+                for line_no, line in enumerate(lines):
+                    if line == '' or line =='\n':
+                        pass
+                    else:
+                        if line[0] == ';' or line[0] == '#':
+                            pass
+                        else:
+                            tsv_file += str(line_no) + "\t" + line
+                            
+            with open(os.path.join(output_folder, file + '.tsv'), encoding='utf-8', mode="w") as g:
+                g.write(tsv_file)
+
+            
