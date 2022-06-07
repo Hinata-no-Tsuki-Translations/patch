194f4893f62330503cc07224000763e6054fef36
Yuu <105552059+yuuyuki@users.noreply.github.com>
2022-06-07 12:34:53 +0800

    Extract to handle line differences

--- extractXlsx/extract.py
+++ extractXlsx/extract.py
@@ -6,6 +6,7 @@ import glob
 jpn_folder = "jpn"
 eng_folder = "eng"
 out_folder = 'out'
+same_length = True
 
 files = os.listdir(jpn_folder)
 
@@ -35,16 +36,27 @@ for file in files:
         with open(eng_file, encoding='shift_jis') as g:
             eng_lines = g.readlines()
         if len(eng_lines) != len(jpn_lines):
-            print("Unequal number of lines for japan and english for " + file + ". English not parsed.")
+            print("Unequal number of lines for japan and english for " + file + ". Please check this file later.")
+            same_length = False
 
+
+    eng_line_pointer = 0
     for line_no, line in enumerate(jpn_lines):
         if not line or line == '' or line =='\n' or line.isspace() or line[0] == ';' or line[0] == '#':
             pass
         else:
             d['Line'].append(line_no)
             d['Jpn'].append(line.strip())
-            if eng_lines:
+            if eng_lines and same_length:
                 d['Eng'].append(eng_lines[line_no].strip())
+            elif eng_lines and not same_length:
+                while eng_line_pointer < len(eng_lines):
+                    eng_line = eng_lines[eng_line_pointer]
+                    if not eng_line or eng_line == '' or eng_line =='\n' or eng_line.isspace() or eng_line[0] == ';' or eng_line[0] == '#':
+                        eng_line_pointer += 1
+                    else:
+                        d['Eng'].append(eng_line.strip())
+                        break
             else:
                 d['Eng'].append('')  # machine translate
                 pass
